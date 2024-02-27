23374b4b2c6d39e13cbed8c0b3496fc8f94caa78
Carlos García Pérez <carlosziok@gmail.com>
2024-02-27 21:52:16 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Carlos Garcia Perez on 27/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
