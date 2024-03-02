9e411dc69ae77159508d6939204e23fbdd790f44
Carlos García Pérez <carlosziok@gmail.com>
2024-03-02 17:55:28 +0300

    Added DataStore

--- ContactList/Models/Model.swift
+++ ContactList/Models/Model.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Carlos Garcia Perez on 27/2/24.
 //
-
+import Foundation
 
 struct Contacts {
     
@@ -16,77 +16,28 @@ struct Contacts {
     var fullName: String {
         "\(name) \(surname)"
     }
-}
-
-let names = [
-    "John",
-    "Aaron",
-    "Tim",
-    "Ted",
-    "Steven",
-    "Jake",
-    "Laura",
-    "Victoria",
-    "Jenny",
-    "Ann"
-]
-let surnames = [
-    "Smith",
-    "Dow",
-    "Isaacson",
-    "Pennyworth",
-    "Jankins",
-    "Sullivan",
-    "Carter",
-    "Thomas",
-    "Hooks",
-    "Braham"
-]
-let phoneNumbers = [
-    "+7984345634",
-    "+7984476562",
-    "+7984443345",
-    "+7996665542",
-    "+7988564532",
-    "+7889345654",
-    "+7986865532",
-    "+7995435121",
-    "+7899888555",
-    "+7978765654"
-]
-let emails = [
-    "underground@gmail.com",
-    "rockstar@mail.ru",
-    "plainboing@yandex.ru",
-    "bennybenassi@gmail.com",
-    "springrose2@gmail.com",
-    "speedyrat@yandex.ru",
-    "balandir@mail.ru",
-    "balrog@gmail.com",
-    "bulldograge@yandex.ru",
-    "cobainjam@mail.ru"
-]
 
 func getContacts() -> [Contacts] {
     var contacts: [Contacts] = []
     
-    let shuffledNames = names.shuffled()
-    let shuffledSurnames = surnames.shuffled()
+    let dataStore = DataStore.shared
     
-    let count = min(10, min(shuffledNames.count, shuffledSurnames.count))
+    let shuffledNames = dataStore.names.shuffled()
+    let shuffledSurnames = dataStore.surnames.shuffled()
+    let shuffledPhoneNumbers = dataStore.phoneNumbers.shuffled()
+    let shuffledEmails = dataStore.emails.shuffled()
+    
+    let count = min(shuffledNames.count, shuffledSurnames.count, shuffledPhoneNumbers.count, shuffledEmails.count)
     
     for index in 0..<count {
-        let randomPhone = phoneNumbers[Int.random(in: 0..<phoneNumbers.count)]
-        let randomEmail = emails[Int.random(in: 0..<emails.count)]
-        
         let contact = Contacts(
             name: shuffledNames[index],
             surname: shuffledSurnames[index],
-            phoneNumber: randomPhone,
-            email: randomEmail
+            phoneNumber: shuffledPhoneNumbers[index],
+            email: shuffledEmails[index]
         )
         contacts.append(contact)
     }
     return contacts
 }
-
+}
---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,64 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Carlos Garcia Perez on 2/3/24.
+//
+
+import Foundation
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let names = [
+        "John",
+        "Aaron",
+        "Tim",
+        "Ted",
+        "Steven",
+        "Jake",
+        "Laura",
+        "Victoria",
+        "Jenny",
+        "Ann"
+    ]
+    let surnames = [
+        "Smith",
+        "Dow",
+        "Isaacson",
+        "Pennyworth",
+        "Jankins",
+        "Sullivan",
+        "Carter",
+        "Thomas",
+        "Hooks",
+        "Braham"
+    ]
+    let phoneNumbers = [
+        "+7984345634",
+        "+7984476562",
+        "+7984443345",
+        "+7996665542",
+        "+7988564532",
+        "+7889345654",
+        "+7986865532",
+        "+7995435121",
+        "+7899888555",
+        "+7978765654"
+    ]
+    let emails = [
+        "underground@gmail.com",
+        "rockstar@mail.ru",
+        "plainboing@yandex.ru",
+        "bennybenassi@gmail.com",
+        "springrose2@gmail.com",
+        "speedyrat@yandex.ru",
+        "balandir@mail.ru",
+        "balrog@gmail.com",
+        "bulldograge@yandex.ru",
+        "cobainjam@mail.ru"
+    ]
+    
+    private init() {}
+}
